555892718975d4e38f8db79460e286e0e2c07b0b
lehoangthienan <lehoangthienan@gmail.com>
2018-04-23 21:09:17 +0700

    update them chuc nang va file php

---
+++ footer.php
@@ -0,0 +1,17 @@
+<div id="bg_footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="footer">
+                    <p>Địa chỉ:&nbsp;Nghệ An</p>
+                    <p>Điện thoại:&nbsp;0949930259</p>
+                    <p>Email:dvdung97@gmail.com</p>                    
+                </div>
+            </div>
+        </div>
+        <script src="../js/jquery.js"></script>
+        <script type="text/javascript" src="ckeditor/ckeditor.js"></script>
+        <script type="text/javascript" src="ckfinder/ckfinder.js"></script>
+        <script type="text/javascript">
+            CKEDITOR.replace('noidung');
+        </script>
+	</div>
