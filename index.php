836904ce0dabd59820144e04635792273b391dc3
lehoangthienan <lehoangthienan@gmail.com>
2018-04-23 21:05:20 +0700

    update index

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include('header.php'); ?>
+<?php include('slider.php'); ?>
+<div class="row">
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-3" id="left">
+		<?php include('left.php'); ?>
+	</div>
+</div>
+<?php include('footer.php'); ?>
