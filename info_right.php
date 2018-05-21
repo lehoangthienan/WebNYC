0c27470a13183326ae90af1927dd4161abfa22f7
nguyenhoang27 <15520257@gm.uit.edu.vn>
2018-05-21 21:37:50 +0700

    update giao diện

--- info_right.php
+++ info_right.php
@@ -9,7 +9,50 @@ $dm_info=mysqli_fetch_assoc($query_a);
      	<p>Chi Tiết Người Bán</p>
      </div>
      <div class="box_main">
-     	<h3 class="sanpham_box_gia" ><?php echo $dm_info['hoten']; ?></h3>
-    	<h4><?php echo $dm_info['dienthoai']; ?></h4>
+     	<div class="detial-name">
+     		<i class="fas fa-user" aria-hidden="true"></i>
+			<span class="name-label">Người bán:</span>
+			<span class="name"><?php echo $dm_info['hoten']?></span></p>
+		</div>
+		<div class="hotline_button">
+               <center>
+			<button class="button numberphone" title="Hotline" aria-label="Hotline"><span><i class="fa fa-shopping-cart" aria-hidden="true"></i> </span> <?php echo $dm_info['dienthoai']?> </button>
+               </center>
+		</div>
+          <div class="detail-adress">
+               <span> <i class="fas fa-address-book" aria-hidden="true"></i>
+               <snap class="adress-label">Địa chỉ:</snap>
+               <snap class="adress"> 284 Lý Thường Kiệt, Phường 14, Quận 10, Hồ Chí Minh. </snap>
+          </div>
+          <div class="detail-date-of-participation">
+               <i class="fas fa-calendar-alt" aria-hidden="true"></i>
+               <snap class="date-of-participation-label">Ngày tham gia:</snap>
+               <snap class="adress"> <?php echo $dm_info['ngaythamgia']?> </snap>
+          </div>
+          <div class="detail-sobaidang">
+               <i class="fas fa-pencil-alt"></i>               
+               <snap class="sobaidang-label">Số bài đăng bán:</snap>
+              <!--  <snap class="adress"> ?php echo $dm_info['sobaidang']?></snap> -->
+          </div>
      </div>
+</div>
+<div class="share">
+     <strong class="text-muted">Chia sẻ tin đăng này cho bạn bè:</strong>
+     <div class="fb-like" data-href="https://www.facebook.com/UIT.Fanpage/" data-layout="button" data-action="like" data-size="small" data-show-faces="true" data-share="true"></div>
+</div>
+<div class="warning">
+          <hr style="margin: 5px 0px 0px;">
+          <h1 class="name">MUA HÀNG AN TOÀN</h1>
+          <li>
+               <em>KHÔNG trả tiền trước.</em>
+          </li>
+          <li>
+               <em>Kiểm tra hàng cẩn thận, đặc biệt với hàng đắt tiền.</em>
+          </li>
+          <li> 
+               <em>Hẹn gặp ở những nơi công cộng.</em>
+          </li>
+          <li>
+               <em>Nếu bạn mua hàng hiệu, hãy gặp mặt tại cửa hàng để nhờ xác minh, tránh mua phải hàng giả.</em>
+          </li>
 </div>
