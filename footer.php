0f693102916e04bbd789dc4e6d11ed43bab6e809
dangdungql <38561127+dangdungql@users.noreply.github.com>
2018-05-28 22:59:46 +0700

    Add files via upload

--- footer.php
+++ footer.php
@@ -9,9 +9,6 @@
             </div>
         </div>
         <script src="../js/jquery.js"></script>
-        <script type="text/javascript" src="js/jquery1.js"></script>
-		<script type="text/javascript" src="js/jquery.js"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
         <script type="text/javascript" src="ckeditor/ckeditor.js"></script>
         <script type="text/javascript" src="ckfinder/ckfinder.js"></script>
         <script type="text/javascript">
