f8e6d3ca79b1acaa9cd079bc01fa5537c8359837
dangdungql <38561127+dangdungql@users.noreply.github.com>
2018-05-28 22:57:23 +0700

    Add files via upload

--- sanphamchitiet.php
+++ sanphamchitiet.php
@@ -47,8 +47,17 @@ if(isset($_GET['id'])&& filter_var($_GET['id'],FILTER_VALIDATE_INT,array('min_ra
 						<div class="bvsanpham_box_detail_container">
 							<h2><?php echo $dm_info['ten']; ?></h2>
 							<div class="sanpham_box_gia">Giá:&nbsp;<?php echo number_format($dm_info['gia'],0,'.','.').'&nbsp;'.$dm_info['donvitinh']; ?></div>
-							<div class="canle"><?php echo $dm_info['tomtat']; ?></div>
-							<div class="canle"><?php echo $dm_info['noidung']; ?></div>
+							<div class="canle1"><?php echo $dm_info['tomtat']; ?></div>
+							<div class="canle2"><?php echo $dm_info['noidung']; ?></div>
+							
+							<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+							<!-- <button id="demo" onclick="myFunction()" class="btnlienhengay"><i class="fa fa-mobile"></i> Liên Hệ Tôi Ngay</button> -->
+<!--							<p id="demo"></p>-->
+							<script>
+								// function myFunction() {
+								// 	document.getElementById("demo").innerHTML = "0985637412";
+								// }
+								</script>
 						</div>
 					</div>
 					<h2>Nhận xét về sản phẩm</h2>
@@ -99,14 +108,14 @@ if(isset($_GET['id'])&& filter_var($_GET['id'],FILTER_VALIDATE_INT,array('min_ra
 							}
 						?>						
 						<div class="form-group">
-							<label style="display:block;">Bình Luận</label>
+							<label style="display:block;">Nhận xét</label>
 							<textarea name="binhluan" style="Width:80%;height:30px;"></textarea>
 						</div>
-						<input type="submit" name="submit" class="btn btn-primary" value="Bình Luận">
+						<input type="submit" name="submit" class="btn btn-primary" value="Nhận Xét">
 					</form>
 					<?php
 					}
-					$query="SELECT * FROM tblbinhluansp,tbluser WHERE tblbinhluansp.iduser=tbluser.id AND 		tblbinhluansp.idbaiviet={$id}";
+					$query="SELECT tblbinhluansp.id,tbluser.hoten,tblbinhluansp.binhluan,tblbinhluansp.ngaydang, tblbinhluansp.giodang,tblbinhluansp.iduser  FROM tblbinhluansp,tbluser WHERE tblbinhluansp.iduser=tbluser.id AND tblbinhluansp.idbaiviet={$id}";
 					$results=mysqli_query($dbc,$query);
 					kt_query($results,$query);
 					while($binhluan=mysqli_fetch_array($results,MYSQLI_ASSOC))
@@ -126,6 +135,52 @@ if(isset($_GET['id'])&& filter_var($_GET['id'],FILTER_VALIDATE_INT,array('min_ra
 							
 					</div>
 					<?php
+					if(isset($_SESSION['admin']))
+					{
+						if($_SESSION['admin']==1||$_SESSION['uid']==$binhluan['iduser'])
+						{
+					?>
+					<a onclick="return confirm('Ban co muon xoa khong')" href="delete_cmt.php?id=<?php echo $binhluan['id'];?>&sp=<?php echo $id;?>">Xóa</a>
+					<a data-toggle="modal" data-target="#myModal<?=$binhluan['id']?>">  Chỉnh Sửa</a>
+					<div class="modal fade" id="myModal<?=$binhluan['id']?>" role="dialog">
+					    <div class="modal-dialog">
+					    
+					      <!-- Modal content-->
+					      	<div class="modal-content">
+					        	<div class="modal-header">
+					          	<button type="button" class="close" data-dismiss="modal">&times;</button>
+					          	<h4 class="modal-title">Bình Luận</h4>
+					        </div>
+					        <div class="modal-body">
+					        	<form name="frmbaiviet" action="edit_comment.php" method="POST">
+									<?php
+										if(isset($message))
+										{
+											echo $message;
+										}
+									?>						
+									<div class="form-group">
+										<p><?php echo $binhluan['hoten'];?></p>
+										<input type="hidden" name="id" value="<?=$binhluan['id']?>" />
+										<input type="hidden" name="product_id" value="<?=$_GET['id']?>" />
+										<textarea name="binhluan" style="Width:80%;height:30px;"><?php echo $binhluan['binhluan'] ?></textarea>
+									</div>
+									<input type="submit" name="submit" class="btn btn-primary" value="Lưu">
+								</form>					          	
+					          	
+					          	
+					        </div>
+					        <!-- <div class="modal-footer">
+					          	<button type="button" class="btn btn-default" data-dismiss="modal">Lưu</button>
+					        </div> -->
+					     	</div>
+					      
+					    </div>
+				  	</div>
+
+					<?php
+						}
+					}
 					}
 
 					if(isset($_SESSION['admin']))
@@ -141,14 +196,13 @@ if(isset($_GET['id'])&& filter_var($_GET['id'],FILTER_VALIDATE_INT,array('min_ra
 						if($_SESSION['uid']==$dm_info['idnguoidang'])
 						{
 						echo '
-						<form method="POST">
-
-						<input class="sanpham_box_order" type="submit" name="dang_bai" class="btn btn-primary" value="Ẩn bài đăng">
-						</form>
-
-						';
+						<div class="admin-buttons">
+						    <form method="POST">
+						        <input class="sanpham_box_order" type="submit" name="dang_bai" class="btn btn-primary" value="Ẩn bài đăng">
+						    </form>';
 						?>
 						<a href="edit_sanpham.php?id=<?php echo $dm_info['id'];?>" class="sanpham_box_order">Sửa bài đăng</a>
+						</div>
 						<?php
 						}
 						if($_SESSION['admin']==1)
@@ -171,6 +225,17 @@ if(isset($_GET['id'])&& filter_var($_GET['id'],FILTER_VALIDATE_INT,array('min_ra
 						}
 					}
 					?>
+					<div class="col-xs-12 margin-top-05"><strong class="text-muted">Chia sẻ tin đăng này cho bạn bè:</strong><hr style="margin: 5px 0px 0px;">
+					<div>
+					<a class="sc-jbKcbu ksqXbD"><img src="https://static.chotot.com.vn/storage/NHA_CDN_PRODUCTION/4b536993b0b8c943580bfdc5b21dbbe0.svg" height="40" width="40"></a>
+					<a class="sc-jbKcbu ksqXbD"><img src="https://static.chotot.com.vn/storage/NHA_CDN_PRODUCTION/7d1ea8f524e9ff3c53d535ca45df6276.svg" height="40" width="40"></a>
+					<a class="zalo-share-button sc-jbKcbu ksqXbD" data-href="https://nha.chotot.com/quan-binh-tan/thue-van-phong-mat-bang-kinh-doanh/nha-nguyen-can-cuc-dep-5x25-3-tam-mat-tien-duong-46368406.htm#xtatc=INT-5-[share_ad_via_zalo]" data-oaid="570044068386186227" data-layout="2" data-color="blue" data-customize="true"><img src="https://static.chotot.com.vn/storage/NHA_CDN_PRODUCTION/d9227dfc7dda59fb965c20bbd6ef1e07.svg" height="40" width="40"><iframe frameborder="0" allowfullscreen="" scrolling="no" width="0px" height="0px" src="https://sp.zalo.me/plugins/share?dev=null&amp;color=blue&amp;oaid=570044068386186227&amp;href=https%3A%2F%2Fnha.chotot.com%2Fquan-binh-tan%2Fthue-van-phong-mat-bang-kinh-doanh%2Fnha-nguyen-can-cuc-dep-5x25-3-tam-mat-tien-duong-46368406.htm%23xtatc%3DINT-5-%5Bshare_ad_via_zalo%5D&amp;layout=2&amp;customize=true&amp;callback=null&amp;id=71d785c3-c1b6-413e-8b9c-abea75d7512d&amp;domain=nha.chotot.com&amp;android=false&amp;ios=false" style="position: absolute;"></iframe></a>
+					<span><a class="sc-jbKcbu ksqXbD"><img src="https://static.chotot.com.vn/storage/NHA_CDN_PRODUCTION/22599a8addb07df5cf3a1d5eaebf8964.svg" height="40" width="40"></a></span>
+					</div>
+					<hr style="margin: 5px 0px 0px;">
+					</div>	
+					
+					<div class="sc-jlyJG iZlQMc"><img class="sc-gipzik fnAfqL" src="https://static.chotot.com.vn/storage/marketplace/shield-iconx4.png" alt="mua bán an toàn"><div class="sc-csuQGl kodETb"><em>Tin rao này đã được kiểm duyệt. Nếu gặp vấn đề, vui lòng báo cáo tin đăng hoặc liên hệ CSKH để được trợ giúp.&nbsp;<a target="_blank" rel="noopener" href="https://www.facebook.com/profile.php?id=100004069660038&fref=hovercard&hc_location=chat">Xem thêm ››</a></em></div></div>
 				</div>
 			</div>
 		</div>
